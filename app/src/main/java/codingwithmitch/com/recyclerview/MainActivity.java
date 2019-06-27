697f61f6755955c9c651f4b221d021da769c7a54
Ajinkya Mandavkar <ajinkyam@fermion.org.in>
2019-06-27 17:06:29 +0530

    third commit

--- app/src/main/java/codingwithmitch/com/recyclerview/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/recyclerview/MainActivity.java
@@ -27,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initImageBitmaps(){
-        Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+        if (testVariable==0) {
+            Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
+        }
 
         mImageUrls.add("https://c1.staticflickr.com/5/4636/25316407448_de5fbf183d_o.jpg");
         mNames.add("Havasu Falls");
